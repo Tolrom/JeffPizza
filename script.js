00166f3f952d78242af9b11c8cafd3ec00a15ed7
Tolrom <max.le.morlot@gmail.com>
2023-11-23 15:08:48 +0100

    Calcul du prix Javascript

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const body = document.querySelector('body'),
     burger = document.querySelector('#burger'),
     nav = document.querySelector('nav'),
-    croixNav = document.querySelector('#croixNav');
+    croixNav = document.querySelector('#croixNav'),
+    price = document.querySelector('.price'),
+    taille = document.querySelector('#taille_pizza'),
+    quanti = document.querySelector('#nb');
+
+
+taille.addEventListener('click', ()=>{
+    calculPrix();
+});
+quanti.addEventListener('click', ()=>{
+    calculPrix();
+});
+
+function calculPrix(){
+    let size = document.querySelector('#taille_pizza').value,
+    quant = document.querySelector('#nb').value,
+    prix;
+    if (size == 'petite'){
+        prix = 10;
+    }
+    if (size == 'moyenne'){
+        prix = 12;
+    }
+    if (size == 'grande'){
+        prix = 14;
+    }
+    prix = prix*quant;
+    price.textContent = ' '+prix+'€';
+}
 
 // OUVERTURE NAV
 burger.addEventListener('click', () => {
